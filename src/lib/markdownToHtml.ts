892010b1de2fbafb64cfa4e5f73d8b83bd7deb07
asheshmagar <asheshmagar8888667@gmail.com>
2024-04-21 09:35:29 +0545

    feat: support highlight code

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,12 @@
-import { remark } from "remark";
-import html from "remark-html";
+import { remark } from 'remark';
+import remarkHighlightjs from 'remark-highlight.js';
+import html from 'remark-html';
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
-  return result.toString();
+	const result = await remark()
+		.use(html, { sanitize: false })
+		// @ts-ignore
+		.use(remarkHighlightjs)
+		.process(markdown);
+	return result.toString();
 }
