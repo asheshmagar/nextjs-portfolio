4aa9539907e6076ab681db4931f74f2b3b3a1cf5
asheshmagar <asheshmagar8888667@gmail.com>
2024-04-21 09:39:01 +0545

    feat: add header component

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,14 +1,23 @@
-import Link from "next/link";
+import Link from 'next/link';
+import { Logo } from './logo';
 
 const Header = () => {
-  return (
-    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
-      <Link href="/" className="hover:underline">
-        Blog
-      </Link>
-      .
-    </h2>
-  );
+	return (
+		<div className="text-center gap-4 justify-between py-4 px-6 flex md:hidden bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 fixed top-0 bg-gray-200 w-full">
+			<Logo />
+			<div className="flex items-center gap-2">
+				{/* <Link href="/about" className="text-sm" prefetch>
+					About
+				</Link> */}
+				<Link href="/posts" className="text-sm" prefetch>
+					Posts
+				</Link>
+				<a href="/resume.pdf" className="text-sm">
+					CV
+				</a>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
