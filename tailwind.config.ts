3e82e01aece9d9e90645680dd55b60450c2223e6
asheshmagar <asheshmagar8888667@gmail.com>
2024-04-21 09:34:43 +0545

    dev: setup tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,43 +1,79 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+const { default: flattenColorPalette } = require('tailwindcss/lib/util/flattenColorPalette');
+const svgToDataUri = require('mini-svg-data-uri');
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      colors: {
-        "accent-1": "#FAFAFA",
-        "accent-2": "#EAEAEA",
-        "accent-7": "#333",
-        success: "#0070f3",
-        cyan: "#79FFE1",
-      },
-      spacing: {
-        28: "7rem",
-      },
-      letterSpacing: {
-        tighter: "-.04em",
-      },
-      fontSize: {
-        "5xl": "2.5rem",
-        "6xl": "2.75rem",
-        "7xl": "4.5rem",
-        "8xl": "6.25rem",
-      },
-      boxShadow: {
-        sm: "0 5px 10px rgba(0, 0, 0, 0.12)",
-        md: "0 8px 30px rgba(0, 0, 0, 0.12)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}'
+	],
+	theme: {
+		extend: {
+			animation: {
+				scroll: 'scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite'
+			},
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+			},
+			colors: {
+				'accent-1': '#FAFAFA',
+				'accent-2': '#EAEAEA',
+				'accent-7': '#333',
+				success: '#0070f3',
+				cyan: '#79FFE1'
+			},
+			spacing: {
+				28: '7rem'
+			},
+			letterSpacing: {
+				tighter: '-.04em'
+			},
+			fontSize: {
+				'5xl': '2.5rem',
+				'6xl': '2.75rem',
+				'7xl': '4.5rem',
+				'8xl': '6.25rem'
+			},
+			boxShadow: {
+				sm: '0 5px 10px rgba(0, 0, 0, 0.12)',
+				md: '0 8px 30px rgba(0, 0, 0, 0.12)'
+			},
+			keyframes: {
+				scroll: {
+					to: {
+						transform: 'translate(calc(-50% - 0.5rem))'
+					}
+				}
+			}
+		}
+	},
+	plugins: [
+		addVariablesForColors,
+		function ({ matchUtilities, theme }: any) {
+			matchUtilities(
+				{
+					'bg-dot-thick': (value: any) => ({
+						backgroundImage: `url("${svgToDataUri(
+							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
+						)}")`
+					})
+				},
+				{ values: flattenColorPalette(theme('backgroundColor')), type: 'color' }
+			);
+		}
+	]
 };
+
+function addVariablesForColors({ addBase, theme }: any) {
+	let allColors = flattenColorPalette(theme('colors'));
+	let newVars = Object.fromEntries(
+		Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+	);
+
+	addBase({
+		':root': newVars
+	});
+}
 export default config;
