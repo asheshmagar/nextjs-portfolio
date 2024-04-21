b92dd68f1079f389b2c6a1353861b3fd8a7923f5
asheshmagar <asheshmagar8888667@gmail.com>
2024-04-21 09:35:44 +0545

    feat: add projects constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,111 @@
-export const EXAMPLE_PATH = "blog-starter";
-export const CMS_NAME = "Markdown";
+export const EXAMPLE_PATH = 'blog-starter';
+export const CMS_NAME = 'Markdown';
 export const HOME_OG_IMAGE_URL =
-  "https://og-image.vercel.app/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg";
+	'https://og-image.vercel.app/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg';
+
+export const PROJECTS = [
+	{
+		title: 'Masteriyo LMS',
+		link: 'https://wordpress.org/plugins/learning-management-system',
+		thumbnail: '/assets/projects/masteriyo.webp'
+	},
+	{
+		title: 'BlockArt Blocks',
+		link: 'https://wordpress.org/plugins/blockart-blocks',
+		thumbnail: '/assets/projects/blockart.webp'
+	},
+	{
+		title: 'Magazine blocks',
+		link: 'https://wordpress.org/plugins/magazine-blocks',
+		thumbnail: '/assets/projects/magazine-blocks.webp'
+	},
+
+	{
+		title: 'Customize My Account Page for WooCommerce',
+		link: 'https://woocommerce.com/products/customize-my-account-for-woocommerce/',
+		thumbnail: '/assets/projects/customize-my-account-page-for-woocommerce.webp'
+	},
+	{
+		title: 'Registration Form Fields for WooCommerce',
+		link: 'https://woocommerce.com/products/registration-form-fields/',
+		thumbnail: '/assets/projects/registration-form-builder.webp'
+	},
+	{
+		title: 'ThemeGrill Demo Importer',
+		link: 'https://wordpress.org/plugins/themegrill-demo-importer',
+		thumbnail: '/assets/projects/themegrill-demo-importer.png'
+	},
+
+	{
+		title: 'ColorMag',
+		link: 'https://wordpress.org/themes/colormag',
+		thumbnail: '/assets/projects/colormag.jpg'
+	},
+	{
+		title: 'Zakra',
+		link: 'https://wordpress.org/themes/zakra',
+		thumbnail: '/assets/projects/zakra.webp'
+	},
+	{
+		title: 'Spacious',
+		link: 'https://wordpress.org/themes/spacious',
+		thumbnail: '/assets/projects/spacious.webp'
+	},
+	{
+		title: 'Flash',
+		link: 'https://wordpress.org/themes/flash',
+		thumbnail: '/assets/projects/flash.webp'
+	},
+	{
+		title: 'Masteriyo LMS',
+		link: 'https://wordpress.org/plugins/learning-management-system',
+		thumbnail: '/assets/projects/masteriyo.webp'
+	},
+	{
+		title: 'BlockArt Blocks',
+		link: 'https://wordpress.org/plugins/blockart-blocks',
+		thumbnail: '/assets/projects/blockart.webp'
+	},
+	{
+		title: 'Magazine blocks',
+		link: 'https://wordpress.org/plugins/magazine-blocks',
+		thumbnail: '/assets/projects/magazine-blocks.webp'
+	},
+
+	{
+		title: 'Customize My Account Page for WooCommerce',
+		link: 'https://woocommerce.com/products/customize-my-account-for-woocommerce/',
+		thumbnail: '/assets/projects/customize-my-account-page-for-woocommerce.webp'
+	},
+	{
+		title: 'Registration Form Fields for WooCommerce',
+		link: 'https://woocommerce.com/products/registration-form-fields/',
+		thumbnail: '/assets/projects/registration-form-builder.webp'
+	},
+	{
+		title: 'ThemeGrill Demo Importer',
+		link: 'https://wordpress.org/plugins/themegrill-demo-importer',
+		thumbnail: '/assets/projects/themegrill-demo-importer.png'
+	},
+
+	{
+		title: 'ColorMag',
+		link: 'https://wordpress.org/themes/colormag',
+		thumbnail: '/assets/projects/colormag.jpg'
+	},
+	{
+		title: 'Zakra',
+		link: 'https://wordpress.org/themes/zakra',
+		thumbnail: '/assets/projects/zakra.webp'
+	},
+	{
+		title: 'Spacious',
+		link: 'https://wordpress.org/themes/spacious',
+		thumbnail: '/assets/projects/spacious.webp'
+	},
+	{
+		title: 'Flash',
+		link: 'https://wordpress.org/themes/flash',
+		thumbnail: '/assets/projects/flash.webp'
+	}
+];
