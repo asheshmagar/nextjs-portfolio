3e2744afd48687b35c01648e328cf4fda4f2216a
asheshmagar <asheshmagar8888667@gmail.com>
2024-04-21 09:44:45 +0545

    feat: redesign home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,51 @@
-import Container from "@/app/_components/container";
-import { HeroPost } from "@/app/_components/hero-post";
-import { Intro } from "@/app/_components/intro";
-import { MoreStories } from "@/app/_components/more-stories";
-import { getAllPosts } from "@/lib/api";
+'use client';
+
+import { motion } from 'framer-motion';
+import { PROJECTS } from '../lib/constants';
+import { HeroParallax } from './_components/hero-parallax';
 
 export default function Index() {
-  const allPosts = getAllPosts();
-
-  const heroPost = allPosts[0];
-
-  const morePosts = allPosts.slice(1);
-
-  return (
-    <main>
-      <Container>
-        <Intro />
-        <HeroPost
-          title={heroPost.title}
-          coverImage={heroPost.coverImage}
-          date={heroPost.date}
-          author={heroPost.author}
-          slug={heroPost.slug}
-          excerpt={heroPost.excerpt}
-        />
-        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-      </Container>
-    </main>
-  );
+	return (
+		<>
+			<motion.div
+				initial={{
+					filter: 'blur(5px)',
+					y: -15
+				}}
+				animate={{
+					y: 0,
+					filter: 'blur(0px)',
+					transition: { type: 'spring', duration: 0.7 }
+				}}
+				className="flex flex-col"
+			>
+				<div>
+					<h1 className="text-lg md:text-3xl flex items-end font-semibold bg-gradient-to-r from-gray-400 to-gray-700 bg-clip-text text-transparent">
+						Hey, I'm{' '}
+						<ruby className="ml-4">
+							Ashesh <rt>अशेष</rt>
+						</ruby>
+					</h1>
+					<small className="bg-gradient-to-r from-gray-400 to-gray-700 bg-clip-text text-transparent">
+						My name is pronounced as /uh-shesh/
+					</small>
+				</div>
+				<div>
+					<motion.h2 className="mt-8 text-[22px] md:text-[42px] font-semibold text-gray-600 tracking-tight leading-none md:leading-[56px] bg-gradient-to-r from-gray-500 to-gray-800  bg-clip-text text-transparent">
+						Ashesh, a full stack developer based in Kathmandu, currently holds the
+						position of Senior Developer and Team Lead at ThemeGrill Pvt Limited. He
+						brings a wealth of experience in PHP, WordPress, JavaScript, TypeScript,
+						React, and diverse web technologies to his role.
+					</motion.h2>
+				</div>
+				<div className="pt-[40px]">
+					<hr className="mb-6" />
+					<p className="uppercase py-4 text-gray-400 text-sm">Projects</p>
+					<div className="-mx-8 mt-8 mb-20 md:-mx-16">
+						<HeroParallax items={PROJECTS} />
+					</div>
+				</div>
+			</motion.div>
+		</>
+	);
 }
