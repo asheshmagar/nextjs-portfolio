6635d85f22d343fbda0663cda4f49bffa075c475
asheshmagar <asheshmagar8888667@gmail.com>
2024-04-21 09:38:53 +0545

    feat: add footer component

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,32 +1,27 @@
-import Container from "@/app/_components/container";
-import { EXAMPLE_PATH } from "@/lib/constants";
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { cn } from '../../lib/utils';
 
 export function Footer() {
-  return (
-    <footer className="bg-neutral-50 border-t border-neutral-200">
-      <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
-          </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
-              href="https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Read Documentation
-            </a>
-            <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-              className="mx-3 font-bold hover:underline"
-            >
-              View on GitHub
-            </a>
-          </div>
-        </div>
-      </Container>
-    </footer>
-  );
+	const pathname = usePathname();
+	return (
+		<div
+			className={cn('flex p-6 md:px-20 items-center gap-4 justify-between text-gray-500', {
+				hidden: pathname !== '/',
+				flex: pathname === '/'
+			})}
+		>
+			<div className="hidden md:block">Ashesh Thapa</div>
+			<div>{new Date().getFullYear()}</div>
+			<div className="md:hidden flex items-center gap-2">
+				<a href="mailto:ashesh.ido@gmail.com">Mail</a>
+				<a href="https://www.linkedin.com/in/ashesh-ido/">LinkedIn</a>
+				<a href="https://twitter.com/ashesh_ido">Twitter</a>
+				<a href="https://profiles.wordpress.org/asheshmagar/">WordPress.org</a>
+			</div>
+		</div>
+	);
 }
 
 export default Footer;
