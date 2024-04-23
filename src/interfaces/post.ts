998ab3b1d4a82e34b36a3106bbfc838d684a9b70
asheshmagar <asheshmagar8888667@gmail.com>
2024-04-23 19:01:23 +0545

    dev: refactor post schema

--- src/interfaces/post.ts
+++ src/interfaces/post.ts
@@ -1,16 +1,8 @@
-import { type Author } from './author';
-
 export type Post = {
 	slug: string;
 	title: string;
-	date: string;
-	coverImage: string;
-	author: Author;
-	excerpt: string;
-	ogImage: {
-		url: string;
-	};
+	publishedAt: string;
+	summary?: string;
 	content: string;
-	preview?: boolean;
 	tags?: string[];
 };
